138ee47d480c93cefdf2161c6c32da2aa2d609d4
Harsh Raj Ranjan <harshrajranjan2005@gmail.com>
2025-12-15 23:22:06 +0530

    added bookcount variable; defined copy constructor, setData with two and three parameter, and defined a getBookCount static member function

--- Book.cpp
+++ Book.cpp
@@ -1,12 +1,14 @@
 #include "Book.h"
 #include <iostream>
 
+int Book::bookCount=0;
 Book::Book() // Book Constructor outside class
 {
 
     id = 0;
     title = "NA";
     price = 0;
+    bookCount++;
 }
 
 Book::Book(int i, string t, float p)
@@ -14,11 +16,40 @@ Book::Book(int i, string t, float p)
     id = i;
     title = t;
     price = p;
+    bookCount++;
 }
+
+Book::Book(const Book& b)
+{
+    id=b.id;
+    title=b.title;
+    price=b.price;
+    bookCount++;
+}
+
+void Book::setData(int i, string t, float p)
+{
+    id=i;
+    title=t;
+    price=p;
+}
+
+void Book::setData(int i,string t)
+{
+    id=i;
+    title=t;
+    price=0;
+}
+
+int Book::getBookCount()
+{   
+    return bookCount;
+}
+
 
 void Book::display() const
 {
     cout << "Id: " << id
          << ", Title: " << title
-         << ", Price " << price << endl;
+         << ", Price: " << price << endl;
 }
