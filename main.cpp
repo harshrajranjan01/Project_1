803df9c4087e963a797ba65e27907ff2a82ee162
Harsh Raj Ranjan <harshrajranjan2005@gmail.com>
2025-12-15 23:25:46 +0530

    created object b2 with copy constructor, and created a object b3 and passed the value by SetData, and displayed the result

--- main.cpp
+++ main.cpp
@@ -6,5 +6,15 @@ using namespace std;
 int main()
 {
     Book b1(101, "C++ programming", 450);
+    Book b2=b1; //copy constructor
+    Book b3;
+    
+    b3.setData(102,"DSA");
+
     b1.display();
+    b2.display();
+    b3.display();
+
+    cout<<"Total Books: "<<Book::getBookCount()<<endl;
+
 }
