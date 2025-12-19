c9ae3ffa5a3014d982f3be8c7a21bbd84f7a43c3
Harsh Raj Ranjan <harshrajranjan2005@gmail.com>
2025-12-20 00:19:20 +0530

    main Updated

--- main.cpp
+++ main.cpp
@@ -1,25 +1,8 @@
-#include <iostream>
-#include "Library.h"
-
-using namespace std;
+#include "LibraryApp.h"
 
 int main()
 {
-    Library lib;
-
-    lib.addBook(Book(1, "C++", 450));
-    lib.addBook(Book(2, "Dsa", 300));
-    lib.addBook(Book(3, "Python", 500));
-
-    cout << "All Books:" << endl;
-    lib.displayAll();
-
-    cout << "Sorted By Price" << endl;
-    lib.sortByPrice();
-    lib.displayAll();
-
-    cout << "Find Book wiht ID: 2" << endl;
-
-    cout << (lib.findById(2) ? "Book Found" : "Book Not Found") << endl;
+    LibraryApp app;
+    app.run();
     return 0;
 }
